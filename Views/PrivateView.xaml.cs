d88d187f6f7ebb5e86252b9ec751f520eb900db1
Rudolf Hardy Plutarco Aragão <rhpa23@gmail.com>
2022-01-23 13:51:04 -0300

    Start user page UI

--- Views/PrivateView.xaml.cs
+++ Views/PrivateView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace FlightJobsDesktop.Views
 {
@@ -10,6 +14,22 @@ namespace FlightJobsDesktop.Views
         public PrivateView()
         {
             InitializeComponent();
+        }
+
+        private void Avatar_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            bool? result = file.ShowDialog();
+
+            if (result.Value && File.Exists(file.FileName))
+            {
+                ImgAvatar.Source = new BitmapImage(new Uri(file.FileName, UriKind.Absolute));
+            }
+        }
+
+        private void ImgGraduation_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
         }
     }
 }
